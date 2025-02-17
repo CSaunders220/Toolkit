89c3f9ac14428fada8260d45119750cb395a415f
CSaunders220 <christophersaunders@comcast.net>
2025-02-17 17:07:01 -0500

    added ssh script

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author christopher s
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes root's ability to ssh in
+echo "CODE"
