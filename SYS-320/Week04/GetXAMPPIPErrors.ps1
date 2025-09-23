423212423050fd5eb53bf80566e74f6713881f9d
CSaunders220 <christophersaunders@comcast.net>
2025-09-23 16:07:17 -0400

    Added week 4

---
+++ SYS-320/Week04/GetXAMPPIPErrors.ps1
@@ -0,0 +1,14 @@
+﻿$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String '404'
+
+$regex = [regex] "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\ "
+
+$ipsUnorganized = $regex.matches($notfounds)
+
+$ips = @()
+    for($i=0; $i -lt $ipsUnorganized.count; $i++){
+        $ips += [PSCustomObject]@{ "IP" = $ipsUnorganized[$i].value}
+    }
+
+$ipoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+$counts = $ipoftens | Group-Object IP
+$counts | Select-Object Count, Name
---
+++ SYS-320/Week04/WebTrafficFunction.ps1
@@ -0,0 +1,22 @@
+﻿$webpage1 = "/index.html"
+$browser1 = "Chrome"
+$status1 = "200"
+
+function getWebTrafficLogs($webpage, $browser, $status){
+    
+    $logs = Get-Content C:\xampp\apache\logs\access.log
+    
+    $FilteredLogs = @()
+    $FilteredLogs += $logs | Where-Object {$_ -contains "$browser"} | Where-Object {$_ -contains "$status"} | Where-Object {$_ -contains "$webpage"}
+    
+    Write-Host $FilteredLogs
+
+    $FilteredLogs | ForEach-Object {
+        $IP = ($_ -split "\s")[0]
+        $matchestable = @()
+        $matchestable += $IP
+    }
+  return $matchestable
+}
+
+getWebTrafficLogs($webpage1, $browser1, $status1)
