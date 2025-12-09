f2e8a23373e8a65a93758e571e8466c0b67fa554
CSaunders220 <christophersaunders@comcast.net>
2025-12-09 00:17:07 -0500

    Fix01

--- SYS-320/FinalAssignment/reportToHTML.bash
+++ SYS-320/FinalAssignment/reportToHTML.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 report=./report.txt
-echo "<html>\n<body>\nAccess logs with IOC Indicators:\n<table>" > report.html
+echo "<html><body>Access logs with IOC Indicators:<table><th>IP Address</th><th>Timestamp</th><th>Webpage</th>" > report.html
 
 while IFS= read -r line
 do
 echo "<tr>" >> report.html
+echo "<td>" >> report.html
 echo "$line" | sed 's/ /<td>/g' >> report.html
 echo "</tr>" >> report.html
 done < "$report"
